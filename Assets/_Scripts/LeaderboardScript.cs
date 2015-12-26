2ca4229e3f64647142f14cb23a1bc811e581a11c
mimimushi <studio.mimimushi@gmail.com>
2015-12-26 18:43:46 +0800

    Leaderboard

---
+++ Assets/_Scripts/LeaderboardScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderboardScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void returnToGameOver() 
+	{
+		Debug.Log ("returnToGameOver");		
+		Application.LoadLevel ("GameOverScene");
+		
+	}
+}
--- Assets/_Scripts/ScoreScript.cs
+++ Assets/_Scripts/ScoreScript.cs
@@ -5,24 +5,24 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour {
 
 	public static int score;
-	public static int lastScoreNumber;
-	GameObject finalScoreObj;
+	//public static int lastScoreNumber;
+	//GameObject finalScoreObj;
 	Text currentScore;
-	Text lastScore;
+	//Text lastScore;
 
 	// Use this for initialization
 	void Awake () {
-		finalScoreObj = GameObject.Find ("FinalScoreText");
+	//	finalScoreObj = GameObject.Find ("FinalScoreText");
 		currentScore = GetComponent<Text> ();
-		lastScore = finalScoreObj.GetComponent<Text> ();
+	//	lastScore = finalScoreObj.GetComponent<Text> ();
 		score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		currentScore.text = "Score: " + score;
-		lastScoreNumber = score;
-		lastScore.text = "Score: " + lastScoreNumber;
+	//	lastScoreNumber = score;
+	//	lastScore.text = "Score: " + lastScoreNumber;
 
 	}
 	//public void finalScore()
